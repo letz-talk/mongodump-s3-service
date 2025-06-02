3caa2068423f2a9c9d07b61afd5425b100c7b5bc
Allluminum <54303340+Allluminum@users.noreply.github.com>
2025-06-02 19:55:27 +0700

    Update setcron.sh

--- setcron.sh
+++ setcron.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+echo "Setting up cron with expression: $CRON_EXPRESSION"
 echo "$CRON_EXPRESSION bash /usr/local/bin/myawesomescript.sh" > /etc/crontabs/root
+chmod 0644 /etc/crontabs/root
 crond -S -l 0 -f
