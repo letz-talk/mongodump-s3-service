23c892aca183333de139734c59ea8c006c1b0367
Allluminum <54303340+Allluminum@users.noreply.github.com>
2025-06-02 19:44:54 +0700

    Update awesomescript.sh

--- awesomescript.sh
+++ awesomescript.sh
@@ -1,81 +1,40 @@
-#!/bin/bash
+FROM node:alpine
 
-if [[ $KEEP_OLD_FILES_DAYS == "" ]]
-then
+# set working directory
+WORKDIR /usr/local/bin
 
-	if [[ $KEEP_OLD_FILES_MINUTES == "" ]]
-	then
-		echo "skipping old file deletions"
-	else
-		echo "deleting old files"
-		find /usr/files -mmin +$KEEP_OLD_FILES_MINUTES -exec rm -f {} \;
-	fi
+COPY package*.json ./
+COPY awesomescript.sh myawesomescript.sh
+COPY setcron.sh setcron.sh
 
-else
-	echo "deleting old files"
-	find /usr/files -mtime +$KEEP_OLD_FILES_DAYS -exec rm -f {} \;
-fi
+COPY instant.sh instant
 
-if [[ $MONGO_HOST == "" ]]
-then
-	echo "Mongo db host not found"
-	exit 1
-fi
+RUN chmod +x instant
+RUN chmod +x myawesomescript.sh
+RUN chmod +x setcron.sh
 
-if [[ $MONGO_PORT == "" ]]
-then
-	echo "Trying default mongo port: 27017"
-	MONGO_PORT=27017
-fi
+RUN apk add bash
 
-dateString="$(date +%d-%m-%Y-%H_%M_%S)"
-path=/usr/files/
-filename=$(echo mongodump_$dateString.zip)
-echo "Creating" $path$filename
+RUN echo "Install System dependencies" && \
+    apk add --no-cache
 
-args=("--host" $MONGO_HOST "--port" $MONGO_PORT "--archive=$path$filename" "--gzip")
+RUN apk add --no-cache tzdata
 
-if [[ $AUTH_DB == "" ]]
-then
-	echo "Setting default authentication database as 'admin'"
-	AUTH_DB=admin
-fi
+RUN echo "Install MongoDB dependencies" && \
+    apk add \
+    mongodb-tools
 
-if [[ $MONGO_USER != "" && $MONGO_PASSWORD != "" ]]
-then
-	args+=("--username" $MONGO_USER "--password" $MONGO_PASSWORD "--authenticationDatabase=$AUTH_DB" ) 
-else 
-	echo "Skipping user authentication"
-fi
+RUN echo "Install Curl as dependency" && \
+    apk add \
+    curl
 
-awsArgs=($path$filename s3://$BUCKET_NAME/$BUCKET_PATH/$filename)
+RUN echo "Install aws-cli" && \
+    apk add --no-cache aws-cli
 
-export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$DEFAULT_REGION
+RUN npm install
 
-if [[ $ENDPOINT != "" ]]
-then
-	echo "Setting ENDPOINT for s3 api"
-	awsArgs+=("--endpoint $ENDPOINT")
-fi
+COPY . .
 
-echo "Running mongodump and s3 push"
-# echo ${args[@]}
-# echo ${awsArgs[@]}
+RUN rm /var/cache/apk/*
 
-if mongodump ${args[@]} && aws s3 cp ${awsArgs[@]} ; then
-    echo "Backup succeeded"
-	if [[ $HEALTHCHECK_IO_CHECK_URL != "" ]]
-	then
-		curl -m 10 --retry 5 $HEALTHCHECK_IO_CHECK_URL
-	fi
-else
-    echo "Backup failed"
-	if [[ $HEALTHCHECK_IO_CHECK_URL != "" ]]
-	then
-		curl --retry 3 $HEALTHCHECK_IO_CHECK_URL/fail
-	fi
-fi
-
-# node ./deleteOldBackupsFromS3.js
+CMD setcron.sh
