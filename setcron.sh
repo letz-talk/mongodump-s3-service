92c494f3ba332d05e00fc721915945341368c180
allluminum <dmitry@rozhk.ov>
2025-06-06 17:20:33 +0700

    восстановление из бэкапа

--- setcron.sh
+++ setcron.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-# Check if CRON_EXPRESSION is set
-[ -z "$CRON_EXPRESSION" ] && { echo "Error: CRON_EXPRESSION not set"; exit 1; }
-
-# Write cron job to /etc/cron.d/mytask
-echo "$CRON_EXPRESSION root bash /usr/local/bin/awesomescript.sh" > /etc/cron.d/mytask
-
-# Start cron in foreground
-exec cron -f
+#!/bin/sh
+echo "$CRON_EXPRESSION bash /usr/local/bin/awesomescript.sh" > /etc/crontabs/root
+crond -S -l 0 -f
