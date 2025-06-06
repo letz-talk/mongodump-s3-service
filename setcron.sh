f615e469a97f70e37994e558e09466abbb621ff0
allluminum <dmitry@rozhk.ov>
2025-06-06 14:14:57 +0700

    восстановление из бэкапа

--- setcron.sh
+++ setcron.sh
@@ -4,8 +4,7 @@
 [ -z "$CRON_EXPRESSION" ] && { echo "Error: CRON_EXPRESSION not set"; exit 1; }
 
 # Write cron job to /etc/cron.d/mytask
-echo "$CRON_EXPRESSION root bash /usr/local/bin/awesomescript.sh >> /var/log/awesomescript.log 2>&1" > /etc/cron.d/mytask
+echo "$CRON_EXPRESSION root bash /usr/local/bin/awesomescript.sh" > /etc/cron.d/mytask
 
-# Start rsyslog and cron
-service rsyslog start
+# Start cron in foreground
 exec cron -f
